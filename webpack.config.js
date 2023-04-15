ade249fc46523dda4e8bde36d0ef90471597314f
pavel.kuznetsov <pavel.kuznetsov@softswiss.com>
2023-04-15 16:39:20 +0700

    fix: js, types, styles

--- webpack.config.js
+++ webpack.config.js
@@ -6,19 +6,34 @@ module.exports = {
     mode: "production",
     module: {
         rules: [
-            { test: /\.scss$/, use: [ "style-loader", "css-loader", "sass-loader" ] },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
         ]
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.jsx', '.js', '.scss']
     },
-    plugins: [
-        new SassBundleWebpackPlugin({
-            file: path.join(__dirname, 'src/core/styles/utils/index.scss'),
-            type: 'sass',
-            output: {
-                name: 'utils'
-            },
-        }),
-    ],
+    // plugins: [
+    //     new SassBundleWebpackPlugin({
+    //         file: path.join(__dirname, 'src/core/styles/utils/index.scss'),
+    //         type: 'sass',
+    //         output: {
+    //             name: 'utils'
+    //         },
+    //     }),
+    // ],
 }
