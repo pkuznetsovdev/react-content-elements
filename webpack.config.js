b241846757cb27233e85ac8a59f0325b119547e0
pavel.kuznetsov <pavel.kuznetsov@softswiss.com>
2023-04-17 16:37:36 +0700

    add: tsconfig, webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,54 @@
 const path = require('path');
 const SassBundleWebpackPlugin = require('sass-bundle-webpack-plugin');
 
+const PATHS = {
+    ENTRY: './src/index.ts',
+    OUTPUT: './dist'
+};
+
+const WEBPACK = {
+    outputFileName: 'bundle.js',
+};
+
 module.exports = {
-    entry: "./src/index.ts",
+    entry: PATHS.ENTRY,
     mode: "production",
+    target: "web",
+    output: {
+        path: path(__dirname, PATHS.OUTPUT),
+        filename: WEBPACK.outputFileName,
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js', '.scss']
+    },
     module: {
         rules: [
             {
-                test: /\.tsx?$/,
+                test: /\.(ts|tsx)$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
+            },
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env",
+                            "@babel/preset-react",
+                            "@babel/preset-typescript"]
+                    },
+                },
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                exclude: /node_modules/,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                ],
             },
             {
                 test: /\.s[ac]ss$/i,
@@ -22,10 +61,11 @@ module.exports = {
                     "sass-loader",
                 ],
             },
+            {
+                test: /\.(jpe?g|png|svg|ttf)$/i,
+                type: "asset/resource",
+            },
         ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js', '.scss']
     },
     // plugins: [
     //     new SassBundleWebpackPlugin({
